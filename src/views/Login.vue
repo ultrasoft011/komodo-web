9eea9a58e6e231d32c2ad9c0a919fa0bac97407e
ultrasoft011 <ultrasoft.011@gmail.com>
2021-07-21 19:03:06 -0500

    login bootstrap vue

---
+++ src/views/Login.vue
@@ -0,0 +1,53 @@
+<template>
+  <div>
+    <h1>Login</h1>
+    <label>Email: </label>
+    <b-form-input type="email" v-model="email" /><br />
+    <label>Password: </label>
+    <b-form-input type="password" v-model="password" /><br />
+    <b-button variant="outline-primary" @click='login()'>Log in</b-button>
+    <router-link to="/register">Sign up</router-link>
+    <p>{{ getData }}</p>
+  </div>
+</template>
+
+<script>
+const axios = require("axios");
+export default {
+  name: "Login",
+  data() {
+    return {
+      email: "",
+      password: "",
+      getData: {},
+    };
+  },
+  methods: {
+    async login() {
+      // console.log(`${this.email} ${this.password}`)
+      const dataGet = await axios.get(
+        "https://jsonplaceholder.typicode.com/todos/1"
+      );
+      console.log(dataGet);
+      this.getData = dataGet.data;
+    },
+  },
+};
+</script>
+
+<style>
+@import url("https://fonts.googleapis.com/css2?family=Monoton&display=swap");
+div {
+  margin: 60px;
+  padding: 50px;
+  background: linear-gradient(
+    90deg,
+    rgba(255, 180, 178, 1) 0%,
+    rgba(255, 238, 238, 1) 100%
+  );
+}
+h1 {
+  font-family: "Monoton", cursive;
+  color: rgb(70, 62, 62);
+}
+</style>
---
+++ src/views/Register.vue
@@ -0,0 +1,47 @@
+<template>
+  <div>
+    <h1>Register</h1>
+    <input type="text" v-model="name" />
+    <input type="text" v-model="username" />
+    <input type="email" v-model="email" />
+    <input type="password" v-model="password" />
+    <button @click="register()">Sign up</button>
+    <router-link to="/login">Log in</router-link>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+export default {
+  name: "Login",
+  data() {
+    return {
+      name: "Andrés",
+      username: "ultrasoft011",
+      email: "ultrasoft.011@gmail.com",
+      password: "Gali1271*+",
+    };
+  },
+  methods: {
+    click() {
+      console.log(`${this.email} ${this.password}`);
+    },
+    async register() {
+      const user = {
+        name: this.name,
+        username: this.username,
+        email: this.email,
+        password: this.password,
+      };
+      console.log("Usuario: ", user);
+      const userRegister = await axios.post(
+        "http://localhost:3000/api/users/register",
+        user
+      );
+      console.log("Imprimir usuario en la DB", userRegister);
+    },
+  },
+};
+</script>
+
+<style></style>
